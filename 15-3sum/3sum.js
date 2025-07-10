9ef55b2958fd3b387d91fd126acf95a0cb262f4f
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-07-10 21:08:17 +0530

    Time: 95 ms (13.92%) | Memory: 72.6 MB (48.30%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(arr) {
+    arr.sort((a,b) => a - b);
+    let ans = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] != arr[i-1]) {
+            twoSum(arr, i, ans);
+        }
+    }
+    console.log(ans);
+    return ans;
+};
+
+let twoSum = (arr, x, ans) => {
+    let i = x + 1, j = arr.length - 1;
+
+    while(i < j) {
+        let sum = arr[i] + arr[j] + arr[x];
+        if(sum == 0) {
+            ans.push([arr[i], arr[j], arr[x]]);
+            ++i, --j;
+            while( i < j && arr[i] == arr[i - 1]) ++i;
+        }else if( sum > 0) {
+            --j;
+        }else {
+            ++i;
+        }
+    }
+}
