787f5351ac2ff6ce4d078739ea3be1d97239814f
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-07-12 18:20:37 +0530

    Time: 232 ms (6.91%) | Memory: 57.5 MB (26.12%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -4,41 +4,36 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    let count = 0, maxSS = 0, i = 0, j = 0;
-    let n = s.length;
     let map = {};
-    let maxChar = "";
-    for(let m = 0; m < n; m++) map[s[m]] = 0;
+    map[s[0]] = 1;
+    let maxSS = 0;
+    let i = 0, j = 0;
 
-    while(j < n) {
-        map[s[j]]++;
-        count++;
-        maxChar = maxChar == "" ? s[j] : findMaxChar(map);
-        let val = count - map[maxChar];
-        if(val <= k) {
+    while( j < s.length ) {
+        if(isWindowVali(map,k)) {
+            maxSS = Math.max(maxSS, (j-i) + 1 );
             ++j;
+            map[s[j]] = map[s[j]] ? ++map[s[j]] : 1;
         }else {
-            maxSS = Math.max( (j-i), maxSS );
-            map[s[j]]--;
-            map[s[i]]--;
-            count -= 2;
+            --map[s[i]];
             ++i;
-            if(j + 1 == n) break;
         }
     }
 
-    if( j == n) maxSS = Math.max( ( j - i), maxSS );
     return maxSS;
+
 };
 
-const findMaxChar = (obj) => {
+const isWindowVali = (map, k) => {
+    let total = 0;
     let max = 0;
-    let ch = "";
-    for(let k in obj) {
-        if(obj[k] > max) {
-            max = obj[k];
-            ch = k;
+    for(let i = 0; i < 26; i++) {
+        let ch = String.fromCharCode( i+65 );
+        if(map[ch]) {
+            total += map[ch];
+            max = Math.max( max, map[ch] ); 
         }
     }
-    return ch;
+    
+    return ( total - max ) <= k;
 }
