e40d22aa8e21d6a34f1028bb1036d7fe4adcbf24
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-07-12 17:21:11 +0530

    Time: 143 ms (10.99%) | Memory: 63.3 MB (5.12%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let count = 0, maxSS = 0, i = 0, j = 0;
+    let n = s.length;
+    let map = {};
+    let maxChar = "";
+    for(let m = 0; m < n; m++) map[s[m]] = 0;
+
+    while(j < n) {
+        map[s[j]]++;
+        count++;
+        // if( maxChar == "" || map[s[j]] > map[maxChar] ) maxChar = s[j];
+        maxChar = maxChar == "" ? s[j] : findMaxChar(map);
+        let val = count - map[maxChar];
+        if(val <= k) {
+            ++j;
+        }else {
+            maxSS = Math.max( (j-i), maxSS );
+            map[s[j]]--;
+            map[s[i]]--;
+            count -= 2;
+            ++i;
+        }
+    }
+
+    if( j == n) maxSS = Math.max( ( j - i), maxSS );
+    return maxSS;
+};
+
+const findMaxChar = (obj) => {
+    let max = 0;
+    let ch = "";
+    for(let k in obj) {
+        if(obj[k] > max) {
+            max = obj[k];
+            ch = k;
+        }
+    }
+    return ch;
+}
