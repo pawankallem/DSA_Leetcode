221528f5794b9da03dae04c8c5c084eae6e4641d
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-07-15 10:56:11 +0530

    Time: 38 ms (6.12%) | Memory: 60.1 MB (6.12%) - LeetSync

---
+++ 3396-valid-word/valid-word.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var isValid = function(word) {
+
+    if(word.length < 3) return false;
+
+    let isVowel = false;
+    let isConsonant = false;
+    let isNum = false;
+
+    for(let i = 0; i < word.length; i++) {
+        let ch = word[i];
+        let chCode = ch.charCodeAt(0);
+        console.log(ch);
+        if(chCode >= 65 && chCode < 97 + 26) {
+            let lowerCh = ch.toLowerCase();
+            if(lowerCh == 'a' || lowerCh == 'e' || lowerCh == 'i' || lowerCh == 'o' || lowerCh == 'u') isVowel = true;
+            else isConsonant = true;
+        }else if( ch >= 0 && ch <= 9) {
+            isNum = true;
+        }else {
+            return false;
+        }
+    }
+    console.log("minch : ", isVowel, isConsonant);
+    if(isVowel && isConsonant) return true;
+    return false;
+    
+};
