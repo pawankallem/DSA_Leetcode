2ef71c4e60828f718f674d84fc376a90537c669c
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-07-17 16:29:07 +0530

    Time: 22 ms (0.15%) | Memory: 58.2 MB (5.64%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
@@ -12,15 +12,27 @@
  */
 var postorderTraversal = function(root) {
     if(!root) return [];
-    let s1 = [root];
-    let s2 = [];
+    let s1 = [];
+    let curr = root;
+    let lastVisited = null;
+    let ans = [];
 
-    while(s1.length > 0) {
-        let curr = s1.pop();
-        s2.push(curr.val);
-        curr.left && s1.push(curr.left);
-        curr.right && s1.push(curr.right);
+    while(curr || s1.length > 0 ) {
+        while(curr) {
+            s1.push(curr);
+            curr = curr.left;
+        }
+        curr = s1[s1.length - 1];
+        console.log(curr.val, ans);
+        if(curr.right != null && lastVisited != curr.right) {
+            console.log("here 1")
+            curr = curr.right;
+        }else {
+            ans.push(curr.val);
+            lastVisited = s1.pop();
+            curr = null;
+            console.log("here 2 : ",curr,lastVisited)
+        }
     }
-
-    return s2.reverse();
+    return ans;
 };
