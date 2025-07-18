5e47de2c219718a45536cce149e9da3e3c598b6b
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-07-18 16:15:51 +0530

    Time: 3 ms (11.15%) | Memory: 57.8 MB (54.73%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -12,9 +12,17 @@
  */
 var maxDepth = function(root) {
     if(!root) return 0;
+    let q = [root];
+    let max = 0;
 
-    let leftCount = root.left && maxDepth(root.left) || 0;
-    let rightCount = root.right && maxDepth(root.right) || 0;
-
-    return 1 + Math.max(leftCount, rightCount);
+    while(q.length) {
+        ++max;
+        let levelSize = q.length;
+        for(let i = 0; i < levelSize; i++) {
+            let curr = q.shift();
+            curr.left && q.push(curr.left);
+            curr.right && q.push(curr.right);
+        }
+    }
+    return max;
 };
