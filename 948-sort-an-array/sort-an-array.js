d39462c65b5eca776520ddb76b21d6afdd6fe8b3
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-08-04 00:15:01 +0530

    Time: 122 ms (52.99%) | Memory: 78.8 MB (69.35%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.js
@@ -0,0 +1,75 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function (nums) {
+
+    const heap = new Heap();
+    for (let i = 0; i < nums.length; i++) {
+        heap.insert(nums[i]);
+    }
+    console.log("finished : ", heap);
+    for (let i = nums.length - 1; i >= 0; --i) {
+        nums[i] = heap.getMax();
+    }
+    return nums;
+
+};
+
+class Heap {
+    constructor() {
+        this.heap = [];
+    }
+    getParent(i) {
+        return Math.floor((i - 1) / 2);
+    }
+    getLeftChild(i) {
+        return (2 * i) + 1;
+    }
+    getRightChild(i) {
+        return (2 * i) + 2;
+    }
+    insert(val) {
+        this.heap.push(val);
+        this.heapifyUp(this.heap.length - 1);
+    }
+    heapifyUp(i) {
+        while (i > 0) {
+            const p_index = this.getParent(i);
+            if (this.heap[p_index] < this.heap[i]) {
+                let temp = this.heap[p_index];
+                this.heap[p_index] = this.heap[i];
+                this.heap[i] = temp;
+            }
+            i = p_index;
+        }
+    }
+    heapifyDown(i) {
+
+        let l_child = this.getLeftChild(i);
+        let r_child = this.getRightChild(i);
+        let n = this.heap.length;
+
+        let largest = i;
+        if( l_child < n && this.heap[l_child] > this.heap[largest]) largest = l_child;
+        if( r_child < n && this.heap[r_child] > this.heap[largest]) largest = r_child;
+
+        if( largest != i) {
+            [this.heap[largest], this.heap[i]] = [this.heap[i], this.heap[largest]];
+            this.heapifyDown(largest);
+        }
+    }
+    getMax() {
+        const val = this.heap[0];
+        this.heap[0] = this.heap[this.heap.length - 1];
+        this.heap.pop();
+        this.heapifyDown(0);
+        return val;
+    }
+}
+
+
+
+
+
+
