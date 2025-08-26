6eaefb95212253ab75d1bdd747b25d6c2a2d5f02
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-08-26 11:43:07 +0530

    Time: 40 ms (71.57%) | Memory: 57.4 MB (79.71%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        MaxHeap heap = new MaxHeap();
+        int n = nums.length;
+        for (int i = n - 1; i >= 0; --i) {
+            heap.heapifyDown(nums, i, n);
+        }
+        // heap.print(nums, 0);
+        int index = n - 1;
+        while (index > 0) {
+            int temp = nums[index];
+            nums[index] = nums[0];
+            nums[0] = temp;
+            heap.heapifyDown(nums, 0, index);
+            // heap.print(nums, index);
+            // System.out.println("                 break         ");
+            --index;
+        }
+        return nums[nums.length - k];
+    }
+}
+
+class MaxHeap {
+    public int getParent(int i) {
+        return (i - 1) / 2;
+    }
+
+    public int getLeftChild(int i) {
+        return (2 * i) + 1;
+    }
+
+    public int getRightChild(int i) {
+        return (2 * i) + 2;
+    }
+
+    public void print(int[] nums, int index) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+        }
+        System.out.println("-------------- " + index);
+    }
+
+    public void heapifyDown(int[] nums, int i, int n) {
+        while (i < n) {
+            int l = getLeftChild(i);
+            int r = getRightChild(i);
+            int curr = i;
+            if (l < n && nums[curr] < nums[l])
+                curr = l;
+            if (r < n && nums[curr] < nums[r])
+                curr = r;
+            if (curr != i) {
+                int temp = nums[curr];
+                nums[curr] = nums[i];
+                nums[i] = temp;
+                i = curr;
+            } else {
+                break;
+            }
+        }
+    }
+}
