4a0afe8c69ad2b693bdbe59d78584330839e874b
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-12-15 00:40:06 +0530

    Time: 1 ms (99.88%) | Memory: 94.7 MB (5.52%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -5,9 +5,8 @@ class Solution {
 
         for(int i = 0; i < arr.length; i++) {
             if(arr[index] < arr[i]) {
-                int sum = arr[i] - arr[index];
-                if( sum > maxProfit) {
-                    maxProfit = sum;
+                if( (arr[i] - arr[index]) > maxProfit) {
+                    maxProfit = (arr[i] - arr[index]);
                 }
             }else if( arr[i] < arr[index] ) {
                 index = i;
