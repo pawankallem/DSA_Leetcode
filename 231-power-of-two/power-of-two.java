c3aea7a39b10f1129b8dcf851c50547f0fa71322
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-12-16 23:21:40 +0530

    Time: 1 ms (80.34%) | Memory: 42.5 MB (35.21%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -3,10 +3,7 @@ class Solution {
         return findPower(0, n);
     }
     public boolean findPower(int i, int num) {
-
         double powValue =  Math.pow(2, i);
-        System.out.println("value : " + powValue + " : " + i + " : " + num + " : " + Math.pow(2, i));
-
         if(powValue == num) {
             return true;
         }else if(powValue > num) {
