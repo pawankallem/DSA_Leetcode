914e1d3db48b3ce287a906dc87e27308b10c62f7
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-12-18 01:55:27 +0530

    Time: 30 ms (37.56%) | Memory: 75.6 MB (69.48%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,37 +1,42 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        if( nums.length == 1) return nums; 
-        int[] left = sortArray(split(nums, 0, nums.length / 2));
-        int[] right = sortArray(split(nums, nums.length / 2, nums.length));
-        return merge(left, right);
+        if (nums.length <= 1) {
+            return nums;
+        }
+        int[] leftArr = sortArray(split(nums, 0, nums.length / 2));
+        int[] rightArr = sortArray(split(nums, nums.length / 2, nums.length));
+
+        return merge(leftArr, rightArr);
     }
-    public int[] split( int[] nums, int start, int end) {
-        int n = end - start;
-        int[] arr = new int[n];
+
+    public int[] split(int[] nums, int start, int end) {
+        int[] arr = new int[end - start];
         int i = 0;
-        while(start < end) {
+        while (start < end) {
             arr[i++] = nums[start++];
         }
         return arr;
     }
-    public int[] merge( int[] left, int[] right) {
+
+    public int[] merge(int[] left, int[] right) {
         int[] arr = new int[right.length + left.length];
         int l = 0;
         int r = 0;
         int i = 0;
-        while( l < left.length && r < right.length ) {
-            if( left[l] < right[r] ) {
+        while (l < left.length && r < right.length) {
+            if (left[l] < right[r]) {
                 arr[i++] = left[l++];
-            }else {
+            } else {
                 arr[i++] = right[r++];
             }
         }
-        while( l < left.length ) {
+        while (l < left.length) {
             arr[i++] = left[l++];
         }
-        while( r < right.length ) {
+        while (r < right.length) {
             arr[i++] = right[r++];
         }
         return arr;
     }
+
 }
