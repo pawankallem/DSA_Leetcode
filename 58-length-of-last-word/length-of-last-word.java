9973dac9de4197f22f6c988d58d1fa048687e105
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2025-12-22 23:32:03 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (56.07%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -2,7 +2,6 @@ class Solution {
     public int lengthOfLastWord(String s) {
         int len = 0;
         for(int i = s.length() - 1; i >= 0; i--) {
-            System.out.println(s.charAt(i));
             if(s.charAt(i) == ' ') {
                 if(len != 0) {
                     break;
